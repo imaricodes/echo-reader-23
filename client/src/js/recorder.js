89cac504f8d786e78df05e1179d77a03fa924d6a
Imari Childress <imaricodes@gmail.com>
2023-01-25 17:55:34 -0500

    Working on speech api callback

--- client/src/js/recorder.js
+++ client/src/js/recorder.js
@@ -1,60 +1,130 @@
-import RecordRTC from  'recordrtc'
+// import RecordRTC from  'recordrtc'
+
+
+
+export const startWebMic = (socket) => {
+  console.log ('useEffect running')
+
+  const getUserMediaConstraints = {audio: {
+    channelCount: 1,
+    sampleRate: 16000,
+  }, video: false}
+  
+  const mediaRecorderOptions = {
+  mimeType: 'audio/webm; codecs=opus',
+  
+  }
+  
+  const reader = new FileReader();
+  
+  function mediaRecorderDataAvailable(e) {
+    reader.readAsDataURL(e.data)
+    reader.onload = () => {
+      let base64data = reader.result.split("base64,")[1];
+      console.log(`base64 ${base64data}`)
+      socket.emit('incoming_stream', base64data)
+    }
+  }
+  
+  async  function startRecorder() {
+    console.log('async called')
+    
+    try {
+      console.log('getting devices')
+      await navigator.mediaDevices.getUserMedia(getUserMediaConstraints)
+      
+      // let track = stream.getAudioTracks()[0];
+      // console.log(track.getCapabilities());
+      .then((stream)=> {
+        let mediaRecorder = new MediaRecorder(stream, mediaRecorderOptions); //pass in options
+  
+        mediaRecorder.start(250);
+   
+        console.log(`mediarecorder mime type `, mediaRecorder.mimeType)
+        mediaRecorder.ondataavailable = mediaRecorderDataAvailable;
+  
+      })
+      
+    } catch (error) {
+      //handle error here
+    }
+  
+  }
+    
+    startRecorder()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // export const startWebMic = (socket)  => {
 
 // socket.emit("go", "go")
 // }
 
-let recorder;
+// let recorder;
 
-export const startWebMic = (socket) => {
+// export const startWebMic = (socket) => {
 
-  navigator.mediaDevices
-    .getUserMedia({ audio: true }) //this opens client media (asks permission first)
-    //pass the media stream to RecordRTC object
-    .then((stream) => {
-      recorder = new RecordRTC(stream, {
-        type: "audio",
-        mimeType: "audio/webm;codecs=pcm", 
-        timeSlice: 250, 
-        desiredSampRate: 16000,
-        numberOfAudioChannels: 1, 
-        bufferSize: 4096,
-        audioBitsPerSecond: 128000,
+//   navigator.mediaDevices
+//     .getUserMedia({ audio: true }) //this opens client media (asks permission first)
+//     //pass the media stream to RecordRTC object
+//     .then((stream) => {
+//       recorder = new RecordRTC(stream, {
+//         type: "audio",
+//         mimeType: "audio/webm;codecs=pcm", 
+//         timeSlice: 250, 
+//         desiredSampRate: 16000,
+//         numberOfAudioChannels: 1, 
+//         bufferSize: 4096,
+//         audioBitsPerSecond: 128000,
 
-        ondataavailable: (blob) => {
-          const reader = new FileReader();
+//         ondataavailable: (blob) => {
+//           const reader = new FileReader();
 
-          reader.onload = () => {
-            //convert sream data (blob) audio to base64
-            let base64data = reader.result.split("base64,")[1];
+//           reader.onload = () => {
+//             //convert sream data (blob) audio to base64
+//             let base64data = reader.result.split("base64,")[1];
 
-            // console.log('stream base62', JSON.stringify(base64data))
-            // console.log('stream base62', base64data.split('base64,')[1])
-            // console.log('stream base64',  base64data)
+//             // console.log('stream base62', JSON.stringify(base64data))
+//             // console.log('stream base62', base64data.split('base64,')[1])
+//             // console.log('stream base64',  base64data)
 
-            socket.emit("audioStream", base64data);
-          };
+//             socket.emit("audioStream", base64data);
+//           };
          
-          reader.readAsDataURL(blob);
+//           reader.readAsDataURL(blob);
 
-        },
+//         },
 
-      });
+//       });
 
-      socket.on("stop_recorder", (message) => {
-        recorder.stopRecording();
-        socket.emit("recorder_stopped", "recording stopped")
-        recorder = null;
-      });
+//       socket.on("stop_recorder", (message) => {
+//         recorder.stopRecording();
+//         socket.emit("recorder_stopped", "recording stopped")
+//         recorder = null;
+//       });
 
-      if (recorder) {
-        recorder.startRecording();
-      }
-    })
+//       if (recorder) {
+//         recorder.startRecording();
+//       }
+//     })
 
-    .catch((err) => console.error(err));
-};
+//     .catch((err) => console.error(err));
+// };
 
 
 // export let startWebMic = (socket) => {
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-//https://www.youtube.com/watch?v=djMy4QsPWiI&t=1s
+//EXPRES SERVER SETUP https://www.youtube.com/watch?v=djMy4QsPWiI&t=1s
 const express = require('express')
 const app = express();
 const http = require("http");
@@ -24,7 +24,7 @@ const speech = require('@google-cloud/speech');
 const client = new speech.SpeechClient();
 
 const config = {
-  encoding: 'LINEAR16',
+  encoding: 'WEBM_OPUS',
   sampleRateHertz: 16000,
   languageCode: 'en-US',
 };
@@ -37,13 +37,15 @@ const request = {
 let recognizeStream = null;
 
 
-const speechCallback = () => {
+const speechCallback = (stream) => {
+
+  console.log('SPEECH CALLBACK CALLED')
   let words = stream.results[0].alternatives[0].transcript;
   let wordsArray = words.split(" ");
   console.log("FINAL TRANSCRIPT? :", stream.results[0].isFinal);
-  console.log("FINAL TRANSCRIPT: ", words);
-  console.log("word array length ", wordsArray.length);
-  console.log(JSON.stringify(wordsArray));
+  // console.log("FINAL TRANSCRIPT: ", words);
+  // console.log("word array length ", wordsArray.length);
+  // console.log(JSON.stringify(wordsArray));
 
   // if (stream.results[0].isFinal == true) {
   //   //send signal to stop recordRTC
@@ -53,22 +55,16 @@ const speechCallback = () => {
   // }
 }
 
-
-
-
-/**
- * Calls the Speech-to-Text API on a demo audio file.
- */
-
 io.on("connection", (socket) => {
+
   console.log(`user id ${socket.id}`)
 
   socket.on("start_speech", (data) => {
     console.log('data received ', data)
   })
 
-  socket.on("audioStream", (audio) => {
-    console.log(`audiostream data ${audio}`)
+  socket.on("incoming_stream", (audio) => {
+    // console.log(`incoming stream ${audio}`)
     recognizeStream = client
     .streamingRecognize(request)
     .on('error', err => {
@@ -79,285 +75,14 @@ io.on("connection", (socket) => {
       }
     })
     .on('data', speechCallback);
-  
-  
+
     recognizeStream.write(audio);
+
   });
 
-
 })
-
-
-
 
 
 server.listen(3001, () => {
     console.log('server is running local host 3001')
 })
-
-
-
-
-
-
-
-/** STASH 1 */
-
-
-// const speech = require("@google-cloud/speech");
-
-// const encoding = "LINEAR16";
-// const sampleRateHertz = 16000;
-// const languageCode = "en-US";
-// const streamingLimit = 5000;
-
-// const client = new speech.SpeechClient();
-
-
-
-
-
-
-// const config = {
-//   encoding: encoding,
-//   sampleRateHertz: sampleRateHertz,
-//   languageCode: languageCode,
-//   enableAutomaticPunctuation: false,
-//   single_utterance: true,
-// };
-
-// const request = {
-//   config,
-//   interimResults: true,
-// };
-
-
-// //this is not needed speech callback instantiated  in processClient function
-// // const speechCallback = (stream) => {
-// //   let words = stream.results[0].alternatives[0].transcript;
-// //   let wordsArray = words.split(" ");
-// //   console.log("FINAL TRANSCRIPT? :", stream.results[0].isFinal);
-// //   console.log("FINAL TRANSCRIPT: ", words);
-// //   console.log("word array length ", wordsArray.length);
-// //   console.log(JSON.stringify(wordsArray));
-
-// //   if (stream.results[0].isFinal == true) {
-// //     //send signal to stop recordRTC
-// //     io.emit("killRecorder", "kill recorder");
-
-
-// //     /** 
-// //      * initiate language processing on client, send final result
-// //      * client will send command to kill cloud speech
-// //      */
-// //     // io.emit("startProcessing", words)
-// //     io.emit("startProcessing", words)
-    
-// //   }
-
-// //   // if (wordsArray.length === 2) {
-// //   //   console.log('max words reached')
-// //   //   recognizeStream.end()
-// //   //   //send signal to stop recordRTC
-// //   //   // io.emit("killRecorder", "kill recorder");
-
-// //   //   //initiate language processing on client
-// //   //   // io.emit("startProcessing", words)
-// //   // }
-
-
-  
-// //   console.log("TRANSCRIPT: ", words);
-// //   // process.stdout.write(words);
-// //   process.stdout.clearLine();
-// //   process.stdout.cursorTo(0);
-
-// // };
-
-
-// //handle new connection for testing
-// let procesClientTesting = (socket) => {
-//   const words = "ghis! is, Sentence."
-//   socket.emit("killRecorder", "kill recorder");
-//   socket.emit("startProcessing", words) 
-// }
-
-// //handle new connection
-// let processClient = (socket) => {
-
-//   const speechCallback = (stream) => {
-//     let words = stream.results[0].alternatives[0].transcript;
-//     let wordsArray = words.split(" ");
-//     console.log("FINAL TRANSCRIPT? :", stream.results[0].isFinal);
-//     console.log("FINAL TRANSCRIPT: ", words);
-//     console.log("word array length ", wordsArray.length);
-//     console.log(JSON.stringify(wordsArray));
-  
-//     if (stream.results[0].isFinal == true) {
-//       //send signal to stop recordRTC
-//       socket.emit("killRecorder", "kill recorder");
-  
-//       socket.emit("startProcessing", words) 
-//     }
-//   }
-
-  
-//   let recognizeStream = client
-//   .streamingRecognize(request)
-//   .on("error", (err) => {
-//     if (err.code === 11) {
-//       // restartStream();
-//     } else {
-//       console.error("API request error " + err);
-//     }
-//   })
-//   .on("data", speechCallback);
-
-//   console.log("connected with id: ", socket.id);
-
-  
-//   socket.on("audioStream", (audio) => {
-//     recognizeStream.write(audio);
-//   });
-
-
-//   socket.on("killSpeechClient", (message) => {
-//     console.log(message)
-//     if (recognizeStream) {
-//       console.log('ENDING SPEECH CLIENT...')
-//       recognizeStream.end()
-//       recognizeStream = null;
-//       console.log('SPEECH CLIENT NULLED:  ', recognizeStream)
-//       socket.disconnect()
-//     } else {console.log('CLIENT ALREADY NULL')} 
-    
-//   })
-// }
-
-// let socketsActive = []
-
-// io.on("connection", (socket) => {
-
-//   console.log("connected with FRESH id: ", socket.id);
-
-// //if socket id does not exist, add socket id to array of ids
-// //need a process here that takes the socket
-// socketsActive.push(socket.id)
-
-  
-//   console.log(`socketActive array ${JSON.stringify(socketsActive)}`)
-//   console.log(`socketActive array length ${JSON.stringify(socketsActive.length)}`)
-  
-//   processClient(socket)
-//   // procesClientTesting(socket)
-
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/** STASH 2 */
-
-
-// const encoding = "LINEAR16";
-// const sampleRateHertz = 16000;
-// const languageCode = "en-US";
-// const streamingLimit = 5000;
-
-// const config = {
-//   encoding: encoding,
-//   sampleRateHertz: sampleRateHertz,
-//   languageCode: languageCode,
-//   enableAutomaticPunctuation: false,
-//   single_utterance: true,
-// };
-
-// const request = {
-//   config,
-//   interimResults: true,
-// };
-
-// let recognizeStream
-
-// const client = new speech.SpeechClient();
-// console.log(`client ${client._streamingRecognize}`)
-
-// /** START PASTED FROM GLOUD */
-
-// //handle new connection
-// const speechCallback = (stream) => {
-
-//   console.log('speech callback called')
-//   let words = stream.results[0].alternatives[0].transcript;
-//   let wordsArray = words.split(" ");
-//   console.log("FINAL TRANSCRIPT? :", stream.results[0].isFinal);
-//   console.log("FINAL TRANSCRIPT: ", words);
-//   console.log("word array length ", wordsArray.length);
-//   console.log(JSON.stringify(wordsArray));
-
-//   if (stream.results[0].isFinal == true) {
-
-//     socket.emit("killRecorder", "kill recorder");
-
-//     socket.emit("startProcessing", words) 
-//   }
-// }
-
-
-// io.on("connection", (socket) => {
-
-//   console.log("connected with FRESH id: ", socket.id);
-
-
-
-
-  
-
-//   recognizeStream = client
-//   .streamingRecognize(request)
-//   .on("error", (err) => {
-//     if (err.code === 11) {
-//       // restartStream();
-//     } else {
-//       console.error("API request error " + err);
-//     }
-//   })
-//   .on("data", speechCallback);
-
-//   socket.on("here_comes", data => {
-//     console.log(`data ${data}`)
-//   })
-
-//   socket.on("audioStream", (audio) => {
-
-//     recognizeStream.write(audio);
-//   });
-
-
-// });
-
-
-
-/** END PASTED FROM GLOUD */
-
-
-
-
-
-
